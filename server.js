dd35252dabb6e2cce88d86272c7946c71989f9f7
Kimsreang Phan <kimsreangphan@Kimsreangs-MacBook-Pro.local>
2019-06-18 20:17:15 -0700

    create the const for server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const express = require('express')
+const mongoose = require('mongoose')
+
 
 axios.get('https://www.nytimes.com/')
     .then(({ data }) => {
